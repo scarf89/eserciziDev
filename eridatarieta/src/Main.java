44db8633fb9996fd77e50788295fee41e424606d
scarf89 <phoenixserviceoff@gmail.com>
2025-02-10 16:55:21 +0100

    xx

--- eridatarieta/src/Main.java
+++ eridatarieta/src/Main.java
@@ -1,8 +1,4 @@
-/* Scrivere un programma che contenga una classe chiamata Animale ed
-un metodo chiamato faiIlVerso() che stampi il verso dell'animale.
-Crea poi una sottoclasse chiamata Gatto che fa override del metodo faiIlVerso()
-per miagolare. Prova quindi a far stampare il verso di Animale e di Gatto.
- */
+
 
 
 class Animale {
